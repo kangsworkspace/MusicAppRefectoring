668fcea2811b9fc7d5fa1cf6de7e1be510f9ad47
kangsworkspace <141600830+kangsworkspace@users.noreply.github.com>
2023-10-06 13:41:57 +0900

    Initial Commit

---
+++ MusicAppRefectoring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicAppRefectoring
+//
+//  Created by Kang on 2023/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
